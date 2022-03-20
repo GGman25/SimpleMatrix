89802f206ee9e9fb6160f91ac79b6200ac0177b1
Gosha <ggman2555@gmail.com>
2022-03-20 11:33:59 +0300

    A simple matrix in CMD

---
+++ matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+title Matrix
+color 0a
+:start
+echo                   %random%   %random%%random%%random%%random%   %random%    %random%-%random%   %random%   %random%-%random%   %random%%random% - %random% 
+goto start
